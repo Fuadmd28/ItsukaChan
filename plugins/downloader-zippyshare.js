8f3ed7fcf66ce47eb998262220f227c09b36aea6
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2022-12-10 17:04:10 +0700

    Update downloader-zippyshare.js

--- plugins/downloader-zippyshare.js
+++ plugins/downloader-zippyshare.js
@@ -1,17 +1,17 @@
-let zsExtract = require('zs-extract')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
- if (!args[0]) throw `Uhm...url nya mana?\n\nContoh:\n${usedPrefix + command} https://www25.zippyshare.com/v/RLghKXdR/file.html`
- let res = await zsExtract.extract(args[0])
- let { download, filename } = res
- m.reply(JSON.stringify(res, null, 2))
- m.reply('Mohon tunggu selama *5 menit* bot sedang mendownload file dari _zippyshare_\n\nMinimal File tidak boleh lebih dari *100MB*\nJika lebih dari yang ditentukan, bot tidak akan mengirimkan file..!!')
- conn.sendFile(m.chat, download, filename, filename, m)
-}
+let { extract } = require('zs-extract')
+let { lookup } = require('mime-types')
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw 'Input URL'
+if (!args[0].includes('zippyshare.com')) throw 'Invalid URL'
+await m.reply('Sedang diproses...')
+for (let i = 0; i < args.length; i++) {
+if (!args[i].includes('zippyshare.com/v')) continue
+let res = await extract(args[i])
+let mimetype = await lookup(res.download)
+conn.sendMessage(m.chat, { document: { url: res.download }, fileName: res.filename, mimetype }, { quoted: m })
+} }
+
 handler.help = ['ippyshare'].map(v => 'z' + v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^z(ippydl|ippyshare)$/i
-
-handler.limit = true
+handler.command = /^z(s|ippy(dl|share)?)$/i 
 handler.premium = true
-
-module.exports = handler
