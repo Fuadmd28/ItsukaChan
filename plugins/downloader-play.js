d587cf29b5625bc12e048610d64f8c170ecf9c80
Muhammad Candra <107687970+Chandra-XD@users.noreply.github.com>
2023-03-03 16:13:19 +0700

    fix

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -12,8 +12,8 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   if (!res) throw res.message;
   let size = res.result.size
   if (size > 180000) throw `*File melebihi batas, download sendiri.!!* ☠️\n\nLink: ${res.result.download}`
-  let buttons = [{ buttonText: { displayText: 'Audio' }, buttonId:  `.yta ${url}` }, { buttonText: { displayText: 'Video' }, buttonId:  `.ytv ${url}` }]
-  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: `*Title:* ${title}\n*Published:* ${ago}\n*Duration:* ${duration}\n*Views:* ${views}\n*Description:* ${description}\n*Url:* ${url}`, footer: '_Please select the button below_', buttons }, { quoted: m })
+  let buttons = [{ buttonText: { displayText: 'Video' }, buttonId:  `.ytv ${url}` }]
+  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: `*Title:* ${title}\n*Published:* ${ago}\n*Duration:* ${duration}\n*Views:* ${views}\n*Description:* ${description}\n*Url:* ${url}`, footer: '_Audio on progress..._', buttons }, { quoted: m })
   conn.sendMessage(m.chat, { audio: { url: res.result.download }, mimetype: 'audio/mpeg' }, { quoted: msg })
 }
 handler.help = ['play'].map(v => v + ' <query>')
